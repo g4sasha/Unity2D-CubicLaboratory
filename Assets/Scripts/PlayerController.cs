5f5059c9cf7e715bc29411bdc405d1491d093e23
Alexandr Gyach <as.gyach@gmail.com>
2023-12-13 15:30:40 +0300

    Add flipX

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,19 +2,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D _rb;
-    [SerializeField] private GroundCheck _gc;
+    [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private GroundCheck _groundCheck;
+    [SerializeField] private SpriteRenderer _spriteRender;
     [SerializeField] private float _speed;
     [SerializeField] private float _jumpForce;
 
+    private float _horizontal;
+
     private void Update()
     {
-        _rb.velocity = new Vector2(Input.GetAxis("Horizontal") * _speed, _rb.velocity.y);
+        // Movement
+        _horizontal = Input.GetAxis("Horizontal");
+        _rigidbody.velocity = new Vector2(_horizontal * _speed, _rigidbody.velocity.y);
 
-        if (Input.GetKey(KeyCode.Space) && _gc.IsGrounded)
+        // Flip
+        if (_horizontal > 0f)
         {
-            _rb.velocity = new Vector2(_rb.velocity.x, 0f);
-            _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+            _spriteRender.flipX = false;
+        }
+        else if (_horizontal < 0f)
+        {
+            _spriteRender.flipX = true;
+        }
+
+        // Jump
+        if (Input.GetKey(KeyCode.Space) && _groundCheck.IsGrounded)
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 0f);
+            _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
         }
     }
 }
