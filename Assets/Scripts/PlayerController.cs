3a23a42dfa026a7b56b1baaf6b68e63ca9292c49
Alexandr Gyach <as.gyach@gmail.com>
2023-12-13 16:51:45 +0300

    Add CameraBorders

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Rigidbody2D _rigidbody;
     [SerializeField] private GroundCheck _groundCheck;
     [SerializeField] private SpriteRenderer _spriteRender;
+    [SerializeField] private Camera _camera;
     [SerializeField] private float _speed;
     [SerializeField] private float _jumpForce;
 
     private float _horizontal;
+    private float _borders, _sideBorders;
+
+    private void Awake()
+    {
+        _borders = _camera.orthographicSize;
+        _sideBorders = _camera.orthographicSize * _camera.aspect;
+    }
 
     private void Update()
     {
@@ -32,5 +40,8 @@ public class PlayerController : MonoBehaviour
             _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, 0f);
             _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
         }
+
+        // Camera borders
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -_sideBorders, _sideBorders), Mathf.Clamp(transform.position.y, -_borders, _borders));
     }
 }
